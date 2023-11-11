696ec5b4b23a2a11a305c0c15ed8e80e9f259b52
eric-hall <46264215+eric-hall@users.noreply.github.com>
2023-11-10 19:58:18 -0500

    Fixed missing reference

--- Application/RestaurantReady/api/src/main/java/com/restready/api/RestaurantReadyAPI.java
+++ Application/RestaurantReady/api/src/main/java/com/restready/api/RestaurantReadyAPI.java
@@ -1,6 +1,6 @@
 package com.restready.api;
 
-import com.restaurantready.common.test.CommonModuleAccessTest;
+import com.restready.common.util.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,7 @@ public class RestaurantReadyAPI {
 
     @GetMapping("/hello")
     public String helloWorldPage() {
-        CommonModuleAccessTest.echo("RestaurantReadyAPI: helloWorldPage() called!");
+        Log.info(this, "RestaurantReadyAPI: helloWorldPage() called!");
         return "From RestaurantReadyAPI: Hello world!";
     }
 }
